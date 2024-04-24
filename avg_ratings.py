93374adf143eb3f6160d2eff7d2ccb653a213efc
tanihad <tanihad@yahoo.gr>
2024-04-23 23:59:23 -0400

    Russ god did

--- avg_ratings.py
+++ avg_ratings.py
@@ -3,24 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def tuplediff():
-    # Moving through both the Spotify and Fantana Dataset and finding the average ratings from each
-    # 1. Loop through combined file
-    # 2. For every song ID -- retrieve the corresponding songs popularity rating from the
-    #    spotify dataset and its song rating from the Fantana dataset.
-    # 3. Add each rating to their corresponding rating sum -- while keeping track of the number of ratings
-    # 4. Calculate average rating
-    # Fantana - spotify_id
-    # Final - album_id
-
     combinedDataFrame = pd.read_csv('FINAL_CLEAN_FILE.csv')
-    #fantanaDataFrame = pd.read_csv('Fanta/Music review/albums.csv')
+    # fantanaDataFrame = pd.read_csv('Fanta/Music review/albums.csv')
 
     # Total number of IDs
     total_id = len(combinedDataFrame)
 
     # Starting sums to calculate the average rating for Fantanas and
     averageDifferentialSum = 0
-
+    songSet = set()
 
     for index, row in combinedDataFrame.iterrows():
         # For each album in the Dataframe, look up its ID in the Fantana
@@ -29,19 +20,26 @@ def tuplediff():
 
         spotifyRating = row['Album Rating']
         fantanaRating = row['rating'] * 10
-        averageDifferentialSum += abs(spotifyRating - fantanaRating)
 
-        similarityratio = min(fantanaRating / spotifyRating, spotifyRating / fantanaRating)
+        if currentSpotifyAlbumID not in songSet:
+            print(spotifyRating, fantanaRating)
+            avgValue = (spotifyRating + fantanaRating) / 2
 
-        ratingTuple = (currentSpotifyAlbumID, currentAlbum, similarityratio )
-        print(ratingTuple)
+            # Calculating the percent difference between the Spotify Popularity rating and Fantano's Rating
+            percentDifference = (abs(spotifyRating - fantanaRating) / avgValue) * 100
+            averageDifferentialSum += percentDifference
+            ratingTuple = (currentSpotifyAlbumID, currentAlbum, percentDifference)
+            songSet.add(currentSpotifyAlbumID)
+            print(ratingTuple)
 
-    ratingDifferential = averageDifferentialSum / total_id
-    print("Average Rating Differential: ", ratingDifferential)
+    ratingDifferential = averageDifferentialSum / len(songSet)
+    print("Average Percent Differential: ", ratingDifferential)
+    print("Total Songs: ", len(songSet))
 
-        # Initializing a tuple that stores the spotify Album ID and how and a ratio describing how similar
-        # Anthony Fantana and Spotify rated the album -- the closer the ratio is to 1.0 the more the ratings
-        # 'agreed' with each other
+    # Initializing a tuple that stores the spotify Album ID and how and a ratio describing how similar
+    # Anthony Fantana and Spotify rated the album -- the closer the ratio is to 1.0 the more the ratings
+    # 'agreed' with each other
+
 
 def graphdiffs():
     import pandas as pd
@@ -133,6 +131,7 @@ def genrecomp():
 
 
 if __name__ == '__main__':
+    tuplediff()
     #graphdiffs()
     #graphdiffs1()
     genrecomp()
